ad20e34f822fe2da88333374daf6126f8adc9c18
Jonathan Thinh <jonathan.thinh@gmail.com>
2017-08-03 21:20:39 -0700

    first commit

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection; 
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  });
+};
+
+
+connection.connect();
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', 0);
